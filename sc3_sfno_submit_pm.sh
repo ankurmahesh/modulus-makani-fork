a5ffda540060969dffbc3f4b685096e84fc5a27e
ankurmahesh <mahesh.ankur10@gmail.com>
2025-01-04 17:01:35 -0800

    adding sc3 embed384 submit script

---
+++ sc3_sfno_submit_pm.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -l                                                          
+#SBATCH --time=18:00:00                                                     
+#SBATCH -C 'gpu&hbm80g'
+#SBATCH --account=m4416
+#SBATCH -q regular                                                      
+#SBATCH --nodes=16
+#SBATCH --ntasks-per-node=4                                             
+#SBATCH --gpus-per-node=4                                               
+#SBATCH --cpus-per-task=32
+#SBATCH -J sfno_linear_74chq_sc3_layers8_edim384_dt6h_wstgl2
+#SBATCH --image=amahesh19/modulus-makani:0.1.0-torch_patch-23.11-multicheckpoint
+#SBATCH --module=gpu,nccl-2.18                                          
+#SBATCH -o logs/sfno_linear_74chq_sc3_layers8_edim384_dt6h_wstgl2-%j.out
+#SBATCH --mail-type=begin,end,fail                                      
+#SBATCH --mail-user=amahesh@lbl.gov 
+#SBATCH --array 0-29
+
+export config_file=./config/sfnonet.yaml
+export config='sfno_linear_74chq_sc3_layers8_edim384_dt6h_wstgl2'
+export run_num="v0.1.0-seed$SLURM_ARRAY_TASK_ID"
+
+export FI_MR_CACHE_MONITOR=userfaultfd
+export NCCL_NET_GDR_LEVEL=PHB
+export HDF5_USE_FILE_LOCKING=FALSE
+
+export MASTER_ADDR=$(hostname)
+# Reversing order of GPUs to match default CPU affinities from Slurm
+export CUDA_VISIBLE_DEVICES=3,2,1,0
+
+export level=0
+export multistep=1
+export h_parallel_size=1
+export w_parallel_size=1
+export amp_mode="bf16"
+
+export cmd="python -m makani.train --yaml_config=$config_file --config=$config --run_num=$run_num --amp_mode=$amp_mode  --checkpointing_level=$level --multistep_count=${multistep} --h_parallel_size=${h_parallel_size} --w_parallel_size=${w_parallel_size} --initialization_seed=$SLURM_ARRAY_TASK_ID" 
+
+set -x
+
+srun -u --mpi=pmi2 shifter \
+    bash -c "
+    source export_DDP_vars.sh
+    ${cmd}
+    "
